305f4b1f0e824edb24fd76ee30d6f82db9e1b1a7
Siddha95 <vlrfranchi1@gmail.com>
2025-10-20 17:56:48 +0200

    detail dish puo essere visto solo da utenti loggati

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from .views import CatalogView, CatalogDetailView, IngredientCreateView, DeleteIngredientView, UpdateIngredientView, AmministrazioneView
 
 
-
 urlpatterns = [
+    # Catalog app
     path("", CatalogView.as_view(), name='home'),
     path("details/<int:pk>/", CatalogDetailView.as_view(), name="catalog-detail"),
     path("ingredient/create", IngredientCreateView.as_view(), name="ingredient-create"),
     path("ingredient/del/<str:pk>", DeleteIngredientView.as_view(), name="ingredient-delete"),
     path("ingredient/update/<str:pk>", UpdateIngredientView.as_view(), name="ingredient-update"),
     path("ingredient/list", AmministrazioneView.as_view(), name="ingredient-list"),
-
 ]
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,35 +1,40 @@
-from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dish, Ingredient
-from .forms import IngredientForm
-from django.http import Http404
+from .forms import IngredientForm 
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 
 class CatalogView(ListView):
     model = Dish
 
-class CatalogDetailView(DetailView):
+class CatalogDetailView(LoginRequiredMixin, DetailView):
+    login_url = "/login/"
+    redirect_field_name = "redirect_to"
     model = Dish
 
 class IngredientCreateView(CreateView):
     template_name = "catalog/ingredient_form.html"
     form_class = IngredientForm
-    success_url = "/ingredient/list"
+    success_url = reverse_lazy("ingredient-list")
 
 # view per amministrazione
 class AmministrazioneView(ListView):
     model = Ingredient
 
+
 # view per eliminare ingrediente
 class DeleteIngredientView(DeleteView):
     model = Ingredient
     template_name = "catalog/confirm_delete.html"
+    # Dentro funzioni o metodi di view - reverse()
+    # Dentro attributi di classi,forms, mixins - reverse_lazy()
     success_url = reverse_lazy("ingredient-list")
 
-# view per fare l'upgrade di un ingrediente
 
+# view per fare l'upgrade di un ingrediente
 class UpdateIngredientView(UpdateView):
     model = Ingredient
     form_class = IngredientForm
