1726ad4db0b2e2c97a0dc807e1a3351803a2d1c4
Siddha95 <vlrfranchi1@gmail.com>
2025-11-04 13:38:49 +0100

    contattaci bootstrappata

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -17,16 +17,14 @@ class CartAddView(LoginRequiredMixin, View):
         dish = get_object_or_404(Dish, id=dish_id)
         quantity = int(request.POST.get('quantity', 1))
         #created diventa vera se viene creato il cart
-        cart, created = Cart.objects.get_or_create(user=request.user)
+        cart= Cart.objects.get_or_create(user=request.user)
         cart_dish, dish_created = Cart_dish.objects.get_or_create(
             cart = cart,
             dish = dish,
             defaults={'quantity':quantity}
             )
         #devo inserire nel template un modo per gestire i messaggi
-        if created:
-            messages.info(request, "Hai appena creato un nuovo carrello!")
-
+        
         if not dish_created:
             cart_dish.quantity += 1
             cart_dish.save()
